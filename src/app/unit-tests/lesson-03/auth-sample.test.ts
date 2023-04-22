425e8dc09745bffe8d5d7fff6c649491f5bb1a8a
st-linhnguyen <linh.nguyen@supremetech.vn>
2023-04-22 11:56:42 +0700

    update mock function

--- src/app/unit-tests/lesson-03/auth-sample.test.ts
+++ src/app/unit-tests/lesson-03/auth-sample.test.ts
@@ -1,27 +1,18 @@
-import { describe, test } from '@jest/globals';
+import { describe, test, jest } from '@jest/globals';
 import { AuthStorageService } from '../../core/services/authStorage.service';
 import AuthSample from './auth.sample';
 
-jest.mock('../../core/services/authStorage.service', () => ({
-  AuthStorageService: jest.fn().mockImplementation(() => ({
-    ACCESS_TOKEN: 'token',
-    setToken: jest.fn(),
-    getToken: jest.fn(),
-    removeToken: jest.fn()
-  }))
-}));
-
 describe('AuthSample', () => {
-  let authStorageService: AuthStorageService;
   let authSample: AuthSample;
 
   beforeEach(() => {
     authSample = new AuthSample();
-    authStorageService = new AuthStorageService();
   });
 
   test('getAuthHeader', () => {
-    authSample.getAuthHeader();
-    expect(authStorageService.getToken).toBeCalled();
+    const spyGetToken = jest.spyOn(AuthStorageService.prototype, 'getToken');
+    const token = authSample.getAuthHeader();
+    expect(spyGetToken).toBeCalled();
+    expect(token).toBe(null);
   });
 });
--- src/app/unit-tests/lesson-03/auth.sample.ts
+++ src/app/unit-tests/lesson-03/auth.sample.ts
@@ -8,6 +8,6 @@ export default class AuthSample {
   }
 
   getAuthHeader() {
-    return `Bearer ${this.authStorage.getToken()}`;
+    return this.authStorage.getToken();
   }
 }
