e608820bc249a5826e72d238e4658ff1b297ea3b
st-linhnguyen <linh.nguyen@supremetech.vn>
2023-04-22 09:22:52 +0700

    create unit test for increasing array

--- src/app/unit-tests/lesson-01/array.ts
+++ src/app/unit-tests/lesson-01/array.ts
@@ -5,3 +5,20 @@ export const setArray = (arr: number[], size: number): number[] => {
   }
   return newArr;
 };
+
+export const isDuplicated = (arr: number[]) => {
+  if (arr.length !== new Set(arr).size) {
+    return true;
+  }
+  return false;
+};
+
+export const isConsecutive = (arr: number[], size: number) => {
+  for (let i = 1; i < size; i++) {
+    if(arr[i] !== arr[i - 1] + 1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
